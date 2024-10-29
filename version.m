2063dbadda1b4864a7908f03e0028171404f0ce9
brrkerr <126521208+brrkerr@users.noreply.github.com>
2024-10-30 00:43:15 +0300

    v0.0.2
    %%  Version: v0.0.2
    %   Date: 2024/10/30
    %   Commiter : Burak Er
    %   Changes:
    %       - Blocks are organized
    %       - Blade flapping removed

---
+++ version.m
@@ -0,0 +1,19 @@
+%% Creator: Burak ER
+
+%%  Version: v0.0.2
+%   Date: 2024/10/30
+%   Commiter : Burak Er
+%   Changes:
+%       - Blocks are organized
+%       - Blade flapping removed 
+
+%%  Version: v0.0.1
+%   Date: 2023/10/30
+%   Commiter : Burak Er
+%   Changes:
+%       - Initial version of Quadcopter Project
+%       - Detailed quadcopter model
+%       - Quadcopter Arm Twisting and inclination effect
+%       - Detailed aerodynamic forces and moments
+%       - All arms can have different motors and propellers
+%       - Attitude controller added
